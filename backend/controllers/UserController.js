29386785d8bf42a99c68981dc2710ee343906707
editaaulia <editaaulia@gmail.com>
2022-05-22 16:08:47 +0700

    Api User

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,7 +2,7 @@ import User from "../models/UserModel.js";
 
 export const getUsers = async(req, res) => {
     try {
-        const reponse = await User.findAll();
+        const response = await User.findAll();
         res.status(200).json(response);
     } catch (error) {
         console.log(error.message);
@@ -11,7 +11,7 @@ export const getUsers = async(req, res) => {
 
 export const getUserById = async(req, res) => {
     try {
-        const reponse = await User.findOne({
+        const response = await User.findOne({
             where: {
                 id: req.params.id
             }
@@ -33,7 +33,7 @@ export const createUser = async(req, res) => {
 
 export const updateUser = async(req, res) => {
     try {
-        await User.create(req.body, {
+        await User.update(req.body, {
             where: {
                 id: req.params.id
             }
@@ -46,7 +46,7 @@ export const updateUser = async(req, res) => {
 
 export const deleteUser = async(req, res) => {
     try {
-        await User.create({
+        await User.destroy({
             where: {
                 id: req.params.id
             }
