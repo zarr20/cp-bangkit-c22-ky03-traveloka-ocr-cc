482b90498f4cd3bfcb5da124ddc8408a475bbaea
zarr20 <zarralghifari@gmail.com>
2022-05-26 23:25:19 +0700

    Add login system

--- frontend/src/containers/Login/index.js
+++ frontend/src/containers/Login/index.js
@@ -1,31 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
-  return (
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // Data Palsu
+  const database = [
+    {
+      username: "admin",
+      password: "admin"
+    },
+    {
+      username: "user",
+      password: "user"
+    }
+  ];
+
+  // const errors = {
+  //   uname: "Invalid username or password",
+  //   pass: "Invalid username or password"
+  // };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="alert alert-danger" role="alert">
+        {errorMessages.message}
+      </div>
+    );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+    const userData = database.find((user) => user.username === uname.value);
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // console.log('password salah');
+        setErrorMessages({ name: "", message: "Invalid username or password" });
+
+      } else {
+        // console.log('berhasil');
+        setIsSubmitted(true);
+
+      }
+    } else {
+      // console.log('not found');
+      setErrorMessages({ name: "", message: "Invalid username or password" });
+    }
+  };
+
+  const renderLogin = (
     <div className='d-flex aligns-item-center vh-100'>
       <div className="m-auto text-center" style={{ maxWidth: "350px", width: "100%" }}>
-        <div>
-          <img src="/logo-identify-purple.svg" style={{ "max-height": "45px" }} />
-          <div className='form-floating mt-4'>
-            <input className='form-control rounded-top rounded-0 ' style={{ marginBottom: "-1px" }} />
+        <form onSubmit={handleSubmit}>
+          <img src="/logo-identify-purple.svg" className="mb-4" style={{ "max-height": "45px" }} />
+          {renderErrorMessage("")}
+          <div className='form-floating '>
+            <input className='form-control rounded-top rounded-0 ' style={{ marginBottom: "-1px" }} name="uname" />
             <label for="floatingInput">Username</label>
           </div>
-          <div className='form-floating'>
-            <input className='form-control rounded-bottom rounded-0' />
+          <div className='form-floating mb-3'>
+            <input className='form-control rounded-bottom rounded-0' name="pass" />
             <label for="floatingInput">Password</label>
           </div>
-          <button type="button" class="mt-4 w-100  btn btn-outline-dark fw-bold">Login</button>
-        </div>
-        
+          
+          <button type="submit" class=" w-100  btn btn-outline-dark fw-bold">Login</button>
+        </form>
+
         <div className='d-flex justify-content-center mt-5'>
-          <img src="/assets/img/bangkit-logo.png" style={{ "max-height": "25px" }} />
-          <img src="/assets/img/traveloka-logo.png" style={{ "max-height": "25px" }} />
+          <img src="/assets/img/bangkit-logo.png" style={{ "max-height": "35px" }} />
+          <img src="/assets/img/traveloka-logo.png" style={{ "max-height": "35px" }} />
         </div>
       </div>
-     
-      
     </div>
+  );
 
+  return (
+    <div>
+      {isSubmitted ? <Navigate replace to="/dashboard" /> : renderLogin}
+    </div>
   )
 }
 
