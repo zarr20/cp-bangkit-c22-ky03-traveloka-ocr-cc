fdcd1a70063d7b015ea81f146e6ef1a461f2e135
editaaulia <99244796+editaaulia@users.noreply.github.com>
2022-06-10 18:23:05 +0700

    Update RefreshToken.js

--- backend/controllers/RefreshToken.js
+++ backend/controllers/RefreshToken.js
@@ -1,4 +1,5 @@
-import User from "../Models/UserModel.js";
+import User from "../models/UserModel.js";
+import admin from "../models/adminModel.js";
 import jwt from "jsonwebtoken";
 
 export const refreshToken = async(req, res) => {
@@ -17,6 +18,32 @@ export const refreshToken = async(req, res) => {
             const name = user[0].name;
             const email = user[0].email;
             const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '15s'
+            });
+            res.json({ accessToken });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const refreshToken_admin = async(req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) return res.sendStatus(401);
+        const admin = await admin.findAll({
+            where: {
+                refresh_token: refreshToken
+            }
+        });
+        if (!admin[0]) return res.sendStatus(403);
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            const adminId = admin[0].id;
+            const name = admin[0].name;
+            const email = admin[0].email;
+            const accessToken = jwt.sign({ adminId, name, email }, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '15s'
             });
             res.json({ accessToken });
