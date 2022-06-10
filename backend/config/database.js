b679e40936b049107e997f87fe4d0c14562d9aaa
editaaulia <99244796+editaaulia@users.noreply.github.com>
2022-06-10 07:48:38 +0700

    update db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_new', 'root', '', {
+const db = new Sequelize('ocr_trvlk', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 
